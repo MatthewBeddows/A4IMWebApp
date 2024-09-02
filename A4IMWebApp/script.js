28d09fec0bee524b0621d4288a51f0fbcec7f0c0
MatthewBeddows <114087393+MatthewBeddows@users.noreply.github.com>
2024-09-02 18:51:34 +0100

    Initial Upload

---
+++ A4IMWebApp/script.js
@@ -0,0 +1,16 @@
+function showSystemView() {
+    console.log("Construct button clicked");
+}
+
+function showDocs() {
+    console.log("User Docs button clicked");
+}
+
+function showAbout() {
+    console.log("About button clicked");
+}
+
+function closeApp() {
+    console.log("Exit button clicked");
+    // Add any additional logic to close the application
+}
